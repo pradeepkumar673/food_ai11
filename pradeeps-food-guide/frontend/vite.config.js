f0145c5bbdbf9703e8f057d5869d506515122af5
pradeepkumar673 <pradeepcastleofficial@gmail.comexit>
2026-01-14 16:44:40 +0530

    f

--- pradeeps-food-guide/frontend/vite.config.js
+++ pradeeps-food-guide/frontend/vite.config.js
@@ -1,3 +1,4 @@
+/*
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,5 +12,15 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+}) */
+
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
   },
 })
